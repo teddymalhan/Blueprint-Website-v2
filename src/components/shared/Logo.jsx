d6a45d289450ff4d6a02a494a60db940fd394d2e
alfonsoocampo <fonsy.ocampo@gmail.com>
2026-01-27 20:37:05 -0800

    feat: revamped NavBar using responsive breakpoints + mobile menu. Added NavBar Dark features + temp_hamburger_icon

--- src/components/shared/Logo.jsx
+++ src/components/shared/Logo.jsx
@@ -2,14 +2,30 @@ import {Link} from "react-router-dom";
 import React from "react";
 import { ReactComponent as LogoIcon } from "../../assets/icons/logo.svg";
 
-const Logo = ({colour = '#0177E8'}) => {
+
+
+const Logo = ({isDark}) => {
     return (
         <Link
             to="/"
-            className="flex space-x-2 hover:opacity-80 hover:cursor-pointer"
+            className={`flex space-x-2 items-center w-[156px] h-[60px] rounded-[5px] px-4 transition-all group
+              ${isDark ? 'bg-[#2A2A2A] hover:bg-[#777777] active:bg-[#AAAAAA]' : 'bg-transparent hover:bg-[#0146BE] active:bg-[#002F80]'}
+           
+            `}
         >
-            <LogoIcon style={{ fill: colour }} />
-            <span style={{ color: colour }} className="font-poppins text-lg font-semibold">
+            <LogoIcon className={`
+                ${isDark ? 'fill-white': 'fill-[#0177E8]' }
+                group-hover:fill-white 
+                group-active:fill-white 
+                transition-all`} />
+            <span className={`
+            font-poppins 
+            text-lg font-semibold 
+            ${isDark ? 'text-white' :'text-[#0177E8]' }
+            group-hover:text-white 
+            group-active:text-white 
+            transition-all`
+            }>
                 blueprint
             </span>
         </Link>
@@ -17,3 +33,4 @@ const Logo = ({colour = '#0177E8'}) => {
 }
 
 export default Logo;
+

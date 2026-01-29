97e232e25129adb2fe99a5cbda93d1b225bbb745
fearyfiremast <fearyfiremast@gmail.com>
2026-01-28 16:06:56 -0800

    feat: begin layout of student page

--- src/pages/StudentsPage.jsx
+++ src/pages/StudentsPage.jsx
@@ -1,8 +1,21 @@
 import React from "react";
-import BlankPage from "../components/layout/BlankPage";
+import PageContainer from "../components/layout/PageContainer";
 
 const StudentsPage = () => {
-  return <BlankPage title="Students Page" />;
+  return (
+  <PageContainer>
+    {/* Hero Section */}
+    <section class="m-4">
+      <h1>about us</h1>
+      <h2>Building innovative, tech-based solutions for communities and public welfare is the mission that brings us together.</h2>
+      <p>Our talented members come from diverse cultures, professions, and social backgrounds. With a passion for social good and dedication to creating beautiful technology, our project teams work alongside nonprofits to help them better serve their communities.</p>
+    </section>
+
+    {/* Hero Section */}
+    <section class="m-4">
+
+    </section>
+  </PageContainer>);
 };
 
 export default StudentsPage;

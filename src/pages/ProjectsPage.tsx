030c6c175266b738eb8c6649341cec7571842670
fearyfiremast <fearyfiremast@gmail.com>
2026-03-08 19:35:44 -0700

    Fix: updated import name to be more clear

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PageContainer from "../components/layout/PageContainer";
-import CommonProjectCard from "../components/shared/CommonProjectCard";
+import ProjectCard  from "../components/projects-page/ProjectProjectCard";
 
 const ProjectsPage = () => {
   return (
@@ -12,16 +12,16 @@ const ProjectsPage = () => {
       <section className="m-4">
         <h2>Current projects</h2>
         <div className="flex flex-col gap-6">
-          <CommonProjectCard />
-          <CommonProjectCard />
+          <ProjectCard />
+          <ProjectCard />
         </div>
       </section>
 
       <section className="m-4">
         <h2>Completed projects</h2>
-        <div className="flex flex-row gap-6">
-          <CommonProjectCard />
-          <CommonProjectCard />
+        <div className="flex flex-col gap-6">
+          <ProjectCard />
+          <ProjectCard />
         </div>
       </section>
 
